af03a0d72e7b5a2e9d893e7522fe06575ae42183
Prerna Kothari <prernasureshku.kothari@stonybrook.edu>
2021-02-24 01:43:50 -0800

    Updates

---
+++ precache-manifest.b770a8dfe58474721b8877a55f5351d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "ce4deec77cbd1c4f3634",
+    "url": "/static/js/main.ce4deec7.chunk.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "ce4deec77cbd1c4f3634",
+    "url": "/static/css/main.72e43972.chunk.css"
+  },
+  {
+    "revision": "6a0eba3ab56dd1415049d5adc0eb455f",
+    "url": "/index.html"
+  }
+];
